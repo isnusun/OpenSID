05c3a569c8c816056e5a094593707d70c4562ac8
Eddie Ridwan <eddie.ridwan@amc.org.au>
2016-09-09 21:57:40 +0800

    Perbaiki penampilan tanggal di surat_permohonan_duplikat_surat_nikah

--- donjo-app/models/surat_model.php
+++ donjo-app/models/surat_model.php
@@ -587,7 +587,7 @@
 			// sesuai dengan panduan, atau boleh juga langsung [isian] saja
 			$isian_tanggal = array("berlaku_dari", "berlaku_sampai", "tanggal", "tgl_meninggal",
 				"tanggal_lahir", "tanggallahir_istri", "tanggallahir_suami", "tanggal_mati",
-				"tanggallahir_pasangan", "tgl_lahir_ayah", "tgl_lahir_ibu", "tgl_berakhir_paspor", "tgl_akte_perkawinan", "tgl_perceraian", "tanggallahir","tanggallahir_pelapor", "tgl_lahir", "tanggallahir_ayah", "tanggallahir_ibu", "tgl_lahir_wali");
+				"tanggallahir_pasangan", "tgl_lahir_ayah", "tgl_lahir_ibu", "tgl_berakhir_paspor", "tgl_akte_perkawinan", "tgl_perceraian", "tanggallahir","tanggallahir_pelapor", "tgl_lahir", "tanggallahir_ayah", "tanggallahir_ibu", "tgl_lahir_wali", "tgl_nikah");
 			foreach ($input as $key => $entry){
 				// Isian tanggal diganti dengan format tanggal standar
 				if (in_array($key, $isian_tanggal)){
